7c51c03b9a8e2d2df0ceeaae157d5cb82456062a
Shahzaib Shahbaz <shahzaibshahbaz2002@gmail.com>
2024-11-12 00:30:46 +0500

    made changes in app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SignIn from "./pages/SignIn";
-import modelsImage from "./Assets/photoshootaesthetic.jpeg";
 
 function App() {
   return (
     <>
-      {/* <SignIn imageprop={modelsImage} /> */}
       <SignIn />
     </>
   );
