cf559ac606eb088d940d5db23c0339d047c3efce
Shahzaib Shahbaz <shahzaibshahbaz2002@gmail.com>
2024-11-11 15:54:46 +0500

    added a new directort for pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import SignIn from "./components/SignIn";
+import SignIn from "./pages/SignIn";
 import modelsImage from "./Assets/photoshootaesthetic.jpeg";
 
 function App() {
