2d71593972fa1a5e16dfa1c983b0df6fb2b29159
Shahzaib Shahbaz <shahzaibshahbaz2002@gmail.com>
2024-11-25 12:44:54 +0500

    made changes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,19 +2,24 @@ import logo from "./logo.svg";
 import "./App.css";
 import SignIn from "./pages/SignIn";
 import Product from "./pages/Product";
-import Homepage from "./pages/Homepage";
+import Collections from "./pages/Collections";
 import Footer from "./components/Footer";
 import SignUp from "./pages/SignUp";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ProductCart from "./components/ProductCart";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Navbar />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/collections" element={<Collections />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
     </div>
   );
---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+
+// Import all images
+import kids from "../Assets/kids.jpg";
+import women from "../Assets/women.jpg";
+import men from "../Assets/men.jpg";
+function Home() {
+  // Array of images - Add your actual imported images here
+  const images = [kids, women, men];
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  // Function to go to next image
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  // Auto-advance slideshow every 3 seconds
+  useEffect(() => {
+    const interval = setInterval(nextImage, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className="relative inline-block w-full h-screen">
+        {/* Image container with fade transition */}
+        <img
+          src={images[currentImageIndex]}
+          alt={`Slide ${currentImageIndex + 1}`}
+          className="w-full h-full object-cover transition-opacity duration-500"
+        />
+
+        {/* Slideshow indicators */}
+        <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+          {images.map((_, index) => (
+            <button
+              key={index}
+              className={`w-3 h-3 rounded-full ${
+                index === currentImageIndex ? "bg-white" : "bg-gray-400"
+              }`}
+              onClick={() => setCurrentImageIndex(index)}
+            />
+          ))}
+        </div>
+
+        {/* Shop Now button */}
+        <Link to="/collections">
+          <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black text-6xl font-bold ">
+            Shop Now!
+          </h1>
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default Home;
