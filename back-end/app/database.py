82356ffffb289d9d8cf722d80bf4c1d6708df5ea
Shahzaib Shahbaz <shahzaibshahbaz2002@gmail.com>
2025-03-10 00:31:05 +0500

    some changes

--- back-end/app/database.py
+++ back-end/app/database.py
@@ -2,26 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi import Request
 import certifi
 
-MONGO_DB_URL = "mongodb+srv://admin:iU2eMAqxgccAaUWY@smartwear-cluster.ksawq.mongodb.net/?retryWrites=true&w=majority&appName=smartwear-cluster"
-
-async def ensure_collections(client):
-    """Ensure all required collections exist."""
-    db = client["smartwear"]
-    collections = await db.list_collection_names()
-    
-    required_collections = [
-        "users",
-        "products",
-        "carts",
-        "orders",
-        "wishlist",
-        "refresh_tokens"
-    ]
-    
-    for collection in required_collections:
-        if collection not in collections:
-            await db.create_collection(collection)
-            print(f"Created collection: {collection}")
+MONGO_DB_URL = "mongodb+srv://admin:shahzaib@smartwear-cluster.ksawq.mongodb.net/?retryWrites=true&w=majority&appName=smartwear-cluster"
 
 async def connect_to_mongo():
     """Establish connection to MongoDB Atlas."""
